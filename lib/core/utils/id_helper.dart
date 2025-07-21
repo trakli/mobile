cf3fbde7c1044e80b6e94424fbe7aa80996b8739
Fuh Austin <fuhaustin@gmail.com>
2025-07-21 21:53:25 +0100

    refactor: Id generation to use sha256

--- lib/core/utils/id_helper.dart
+++ lib/core/utils/id_helper.dart
@@ -1,12 +1,23 @@
 // import 'package:device_uuid/device_uuid.dart';
+import 'package:device_uuid/device_uuid.dart';
 import 'package:uuid/uuid.dart';
+import 'package:crypto/crypto.dart';
+import 'dart:convert';
+
+/// Returns a SHA256 hash of the result.
+String hashConcatenatedStrings(String id) {
+  final bytes = utf8.encode(id);
+  final digest = sha256.convert(bytes);
+  return digest.toString();
+}
 
 /// Generates a unique ID by combining the device UUID and a random UUID.
 /// If device UUID is unavailable, returns only the random UUID.
 Future<String> generateDeviceScopedId() async {
-  // final deviceId = await DeviceUuid().getUUID();
+  final deviceId = await DeviceUuid().getUUID();
   final randomId = const Uuid().v4();
 
-  return randomId;
-  // return deviceId != null ? '$deviceId-$randomId' : randomId;
+  // return randomId;
+  final random = deviceId != null ? '$deviceId-$randomId' : randomId;
+  return hashConcatenatedStrings(random);
 }
