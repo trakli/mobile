2bc9a5ba0ce09ef3bb6878135e7e68de19878834
Fuh Austin <fuhaustin@gmail.com>
2026-01-04 12:50:15 +0100

    refactor: Remove un used imports

--- lib/presentation/root/main_navigation_screen.dart
+++ lib/presentation/root/main_navigation_screen.dart
@@ -1,17 +1,14 @@
-import 'package:collection/collection.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:trakli/core/constants/config_constants.dart';
 import 'package:trakli/di/injection.dart';
 import 'package:trakli/core/sync/sync_database.dart';
 import 'package:trakli/gen/assets.gen.dart';
 import 'package:trakli/gen/translations/codegen_loader.g.dart';
 import 'package:trakli/presentation/add_transaction_screen.dart';
 import 'package:trakli/presentation/auth/cubits/auth/auth_cubit.dart';
-import 'package:trakli/presentation/config/cubit/config_cubit.dart';
 import 'package:trakli/presentation/root/bloc/main_navigation_page_cubit.dart';
 import 'package:trakli/presentation/utils/app_navigator.dart';
 import 'package:trakli/presentation/utils/bottom_nav.dart';
