1248454a78680c53f039eb7f86bdde32e88e0f5a
Wuyika <wuyika.serge@gmail.com>
2026-02-02 15:59:11 +0100

    enh(UI): Back button dark theme color

--- lib/presentation/utils/back_button.dart
+++ lib/presentation/utils/back_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:trakli/presentation/utils/app_navigator.dart';
+import 'package:trakli/presentation/utils/colors.dart';
 
 class CustomBackButton extends StatelessWidget {
   const CustomBackButton({super.key});
